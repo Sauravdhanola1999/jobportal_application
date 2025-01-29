fb3c12acb955ab4521115874c044359f04581a5b
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-01-29 12:00:41 +0530

    changes for project

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
-app.get("*", ( _, res) => {
+app.get('*', ( _, res) => {
   res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 });
 
