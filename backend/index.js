c25771e0b39041e8eba4f5e90928817edb8cb1c9
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-01-29 12:52:54 +0530

    api changes

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://jobportal-application-8bwl.onrender.com/",
+  origin: "https://jobportal-application-8bwl.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
