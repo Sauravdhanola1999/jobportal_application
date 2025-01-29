55cc9c446986eb14e1b5674430b83bd64b4da498
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-01-29 12:40:23 +0530

    index issue

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://jobportal-application-8bwl.onrender.com/",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/company", companyRouter);
